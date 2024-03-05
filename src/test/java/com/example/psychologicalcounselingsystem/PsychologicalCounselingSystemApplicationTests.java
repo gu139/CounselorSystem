9a5c63a64a8fa163542e3fdbbdc3b37bd828da99
15013 <1501303528@qq.com>
2024-03-06 00:13:10 +0800

    demo

---
+++ src/test/java/com/example/psychologicalcounselingsystem/PsychologicalCounselingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.psychologicalcounselingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsychologicalCounselingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
