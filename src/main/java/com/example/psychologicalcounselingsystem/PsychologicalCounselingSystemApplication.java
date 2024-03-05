14599a71b80c747936466cbdce5468096533aeee
15013 <1501303528@qq.com>
2024-03-06 00:13:10 +0800

    demo

---
+++ src/main/java/com/example/psychologicalcounselingsystem/PsychologicalCounselingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.psychologicalcounselingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsychologicalCounselingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsychologicalCounselingSystemApplication.class, args);
+    }
+
+}
