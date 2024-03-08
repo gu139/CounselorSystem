16f1f335f8ccd8e7c38758d4cc316e6640572640
15013 <1501303528@qq.com>
2024-03-08 19:27:53 +0800

    0.03 已完成登录,传userid

--- src/main/java/com/example/psychologicalcounselingsystem/mybatis/mapper/UserInfoMapper.java
+++ src/main/java/com/example/psychologicalcounselingsystem/mybatis/mapper/UserInfoMapper.java
@@ -1,6 +1,5 @@
 package com.example.psychologicalcounselingsystem.mybatis.mapper;
 
-import com.example.psychologicalcounselingsystem.mybatis.entity.User;
 import com.example.psychologicalcounselingsystem.mybatis.entity.UserInfo;
 import org.apache.ibatis.annotations.*;
 
@@ -9,17 +8,17 @@ import java.util.List;
 @Mapper
 public interface UserInfoMapper {
     @Select("select * from userinfo where Id = #{id}")
-    @ResultMap("userinfoMap")
+    @ResultMap("userInfoMap")
     UserInfo getUserInfoById(@Param("id") int id);
 
     // 根据userID查询userinfo
     @Select("select * from userinfo where userID = #{userID}")
-    @ResultMap("userinfoMap")
+    @ResultMap("userInfoMap")
     UserInfo getUserInfoByUserID(@Param("userID") int userID);
 
     // 查询所有userinfo
     @Select("select * from userinfo")
-    @ResultMap("userinfoMap")
+    @ResultMap("userInfoMap")
     List<UserInfo> getAllUserInfos();
 
     // 添加userinfo
@@ -29,11 +28,14 @@ public interface UserInfoMapper {
 
     // 修改userinfo
     @Update("update userinfo set  userName = #{userName} where Id = #{id}")
+    @ResultMap("userInfoMap")
     boolean updateUserInfoName(UserInfo userInfo);
 
     @Update("UPDATE userinfo SET userID = #{userInfo.userID}, userName = #{userInfo.userName}, userPhonenum = #{userInfo.userPhonenum}, userProfect = #{userInfo.userProfect}, userLocation = #{userInfo.userLocation}, userAddress = #{userInfo.userAddress} WHERE id = #{userInfo.id}")
+    @ResultMap("userInfoMap")
     boolean updateUserInfo(@Param("userInfo") UserInfo userInfo);
     // 删除userinfo
     @Delete("delete from userinfo where Id = #{id}")
+    @ResultMap("userInfoMap")
     boolean deleteUserInfo(@Param("id") int id);
 }
